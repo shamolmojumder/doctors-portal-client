e75a629917a85159c36d546af4693f490ed7081f
shamolmojumder <shamolmojumder14@gmail.com>
2021-12-08 19:34:22 +0600

    51-6 set services and related data with style

--- src/components/Home/ServiceDetail/ServiceDetail.js
+++ src/components/Home/ServiceDetail/ServiceDetail.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const ServiceDetail = () => {
+const ServiceDetail = ({service}) => {
     return (
-        <div>
-            
+        <div className="col-md-4 text-center">
+            <img style={{height:'50px'}} src={service.img} alt="" />
+            <h5 className="mt-3 mb-3">{service.name} </h5>
+            <p className="text-secondary">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat, vitae.</p>
         </div>
     );
 };
--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -2,6 +2,7 @@ import React from 'react';
 import fluoride from '../../../images/fluoride.png'
 import cavity from '../../../images/cavity.png'
 import whitening from '../../../images/whitening.png'
+import ServiceDetail from '../ServiceDetail/ServiceDetail';
 const serviceData=[
     {
         name:'Flouride Treatment',
@@ -18,11 +19,20 @@ const serviceData=[
 ]
 const Services = () => {
     return (
-        <section className="services-container">
+        <section className="services-container mt-5">
             <div className="text-center">
                 <h5 style={{color:"#1cc7c1"}}>Our Services</h5>
                 <h2 style={{color:'##3A4056'}}>Services We Provide</h2>
             </div>
+            
+          <div className="d-flex justify-content-center">
+            <div className="w-75 row mt-5 pt-5">
+            {
+                serviceData.map(service=> <ServiceDetail service={service}></ServiceDetail> )
+            }
+            </div>
+        </div>
+
         </section>
     );
 };
