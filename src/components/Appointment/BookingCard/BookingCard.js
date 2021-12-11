7ce0d9484593fbf40cb4ab32e78ccfd87460a2ff
shamolmojumder <shamolmojumder14@gmail.com>
2021-12-12 01:35:46 +0600

    52-4 Display a modal from on appointment click

--- src/components/Appointment/BookingCard/BookingCard.js
+++ src/components/Appointment/BookingCard/BookingCard.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AppointmentForm from '../AppointmentForm/AppointmentForm';
 
 const BookingCard = ({booking}) => {
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+    function closeModal() {
+        setIsOpen(false);
+    }
     return (
         <div className='col-md-4 mb-5'>
             <div className="card p-3">
@@ -8,7 +17,8 @@ const BookingCard = ({booking}) => {
                     <h5 className="card-title text-brand"> {booking.subject}</h5>
                     <h6> {booking.visitingHour} </h6>
                     <p>{booking.totalSpace} Spaces avaiable </p>
-                    <button className='btn btn-brand text-uppercase'>Book Appointment</button>
+                    <button onClick={openModal} className='btn btn-brand text-uppercase'>Book Appointment</button>
+                    <AppointmentForm modalIsOpen={modalIsOpen} closeModal={closeModal} appointmentOn={booking.subject}></AppointmentForm>
                 </div>
             </div>
         </div>
