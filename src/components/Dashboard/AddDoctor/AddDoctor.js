0ae826452f1447fba51bbcf7ff06d20824279a07
shamolmojumder <shamolmojumder14@gmail.com>
2021-12-31 17:07:10 +0600

    52_5-1 Create Admin page to upload image to the server

--- src/components/Dashboard/AddDoctor/AddDoctor.js
+++ src/components/Dashboard/AddDoctor/AddDoctor.js
@@ -2,26 +2,43 @@ import React, { useState } from 'react';
 import Sidebar from '../Sidebar/Sidebar';
 
 const AddDoctor = () => {
-    const [info,setInfo]=useState({})
-    const [file,setFile]=useState(null);
-    const handleBlur=e=>{
-        const newInfo={...info};
-        newInfo[e.target.name]=e.target.value;
+    const [info, setInfo] = useState({})
+    const [file, setFile] = useState(null);
+    const handleBlur = e => {
+        const newInfo = { ...info };
+        newInfo[e.target.name] = e.target.value;
         setInfo(newInfo)
         console.log(info);
     }
-    const handleFileChange=(e)=>{
-        const newFile=e.target.files[0];
+    const handleFileChange = (e) => {
+        const newFile = e.target.files[0];
         setFile(newFile)
         console.log(file);
     }
+    const handleSubmit = () => {
+        const formData = new FormData()
+        formData.append('file', file)
+        formData.append('name', info.name)
+        formData.append('file', info.email)
 
+        fetch('http://localhost:5000/addADoctor', {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
     return (
         <section className='container-fluid row'>
             <Sidebar></Sidebar>
             <div className="col-md-10 p-5 pr-5" style={{ position: 'absolute', right: 0, backgroundColor: "#F4FDFB" }}>
                 <h5 className='text-brand'>Add Doctor</h5>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div class="form-group">
                         <label for="exampleInputEmail1">Email address</label>
                         <input onBlur={handleBlur} type="email" class="form-control" name='email' placeholder="Enter email" />
